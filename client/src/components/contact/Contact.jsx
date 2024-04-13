c1a7f62f0d11f57e0a880e25fe180d405496b32f
tutich <135505990+tutich@users.noreply.github.com>
2024-04-13 14:03:37 +0300

    conact functionality

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import error from '../../images/icon-error.svg'
+import { useState } from 'react';
 
 const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState(true);
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+    setIsValidEmail(validateEmail(e.target.value));
+  };
+
+  const validateEmail = (email) => {
+    // You can implement your email validation logic here
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
   return (
     <div>
       <section>
@@ -9,9 +24,20 @@ const Contact = () => {
         <h3>Stay up-to-date with what we’re doing</h3>
         <div>
           <div className='relative'>
-            <input type="text" placeholder='Enter your email address'/>
-            <img src={error} alt="" className='absolute right-0 top-1/2 transform -translate-y-1/2 mr-2' />
+            <input type="text" placeholder='Enter your email address' value={email}
+               onChange={handleChange}
+              />
+           {!isValidEmail && (
+            <img
+              src={error}
+              alt="Error"
+              className='absolute right-0 top-1/2 transform -translate-y-1/2 mr-2'
+            />
+          )}
           </div>
+          {!isValidEmail && (
+          <p className="text-red-500">Whoops, make sure it's an email</p>
+          )}
           <button type='submit'>Contact Us</button>
         </div>
       </section>
