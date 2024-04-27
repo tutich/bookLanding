38868323ced98ebfd1e456652af7da7afcfe81a2
tutich <135505990+tutich@users.noreply.github.com>
2024-04-27 10:23:20 +0300

    abou css

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
   return (
     <div>
       <section>
-        <article>
+        <article className=''>
           <h2>Features</h2>
           <p>Our aim is to make it quick and easy for you to access your favourite websites.
           Your bookmarks sync between your devices so you can access them on the go.
