5b75860a0c9a9471107777a8b5b96bef3f2cd67f
tutich <135505990+tutich@users.noreply.github.com>
2024-04-27 12:53:03 +0300

    nav jsx,css

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,12 @@ import Links from './components/links/Links';
 import Faq from './components/faq/Faq';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
+import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Intro />
       <About />
       <Links />
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import logo from '../../images/logo-bookmark.svg'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+      <section className='mx-auto justify-between mb-12  py-6 flex lg:w-[80%]'>
+        <div>
+          <img src={logo} alt="" />
+        </div>
+        <div className='flex mr-9 items-center  gap-5'>
+          <ul className='flex gap-7'>
+            <li className='text-sm text-gray-500 cursor-pointer'>FEATURES</li>
+            <li className='text-sm text-gray-500 cursor-pointer'>PRICING</li>
+            <li className='text-sm text-gray-500 cursor-pointer'>CONTACT</li>
+          </ul>
+          <button className='bg-red-500 hover:bg-red-700 text-white py- px-2 rounded'>LOGIN</button>
+        </div>
+      </section>
+    </div>
   )
 }
 
